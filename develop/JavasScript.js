93d97145b872132021c3d3364d0f3be81feebeee
Ian Nater <ian.nater@gmail.com>
2020-09-15 19:45:44 -0500

    got 20 day stats basic functionality working

--- develop/JavasScript.js
+++ develop/JavasScript.js
@@ -5,63 +5,89 @@ var searchInput = document.getElementById('statsInput');
 var searchButton = document.getElementById('searchButton');
 var platInput = document.getElementById('platInput');
 
-function getCurrentStats (inputValue, platformValue){
-var settings = {
-    "async": true,
-    "crossDomain": true,
-    "url": `https://call-of-duty-modern-warfare.p.rapidapi.com/warzone/${inputValue}/${platformValue}`,
-    "method": "GET",
-    "headers": {
-        "x-rapidapi-host": "call-of-duty-modern-warfare.p.rapidapi.com",
-        "x-rapidapi-key": "3d89a34d3cmshc035eb7f8e6a3f2p12c4fcjsnea2565e57614"
-    }
-}
+// function for the ajax call to get the current stats and populate them to the table 
 
-$.ajax(settings).then(function (response) {
-    let brRes = response.br
-    var kills = brRes.kills;
-    var kd = brRes.kdRatio;
-    var scorePerMin = brRes.scorePerMinute;
-    var topTen = brRes.topTen;
-    var topFive = brRes.topFive;
-    var wins = brRes.wins
-    var timePlayed = brRes.timePlayed;
-    var statTable = `<tr> 
+function getCurrentStats(inputValue, platformValue) {
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": `https://call-of-duty-modern-warfare.p.rapidapi.com/warzone/${inputValue}/${platformValue}`,
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "call-of-duty-modern-warfare.p.rapidapi.com",
+            "x-rapidapi-key": "3d89a34d3cmshc035eb7f8e6a3f2p12c4fcjsnea2565e57614"
+        }
+    }
+
+    $.ajax(settings).then(function (response) {
+        let brRes = response.br
+        var kills = brRes.kills;
+        var kd = brRes.kdRatio;
+        var kdLimit = kd.toFixed(3)
+        var topTen = brRes.topTen;
+        var topFive = brRes.topFive;
+        var wins = brRes.wins
+        var timePlayed = brRes.timePlayed;
+        var statTable = `<tr> 
     <td> ${inputValue} </td>
     <td> ${timePlayed} </td> 
     <td> ${wins} </td> 
     <td> ${topFive} </td> 
     <td> ${topTen} </td> 
     <td> ${kills} </td> 
-    <td> ${kd} </td>  
-    
+    <td> ${kdLimit} </td>   
+    <td> <button><i class="fas fa-search"></i> </button></td>
     </tr>`;
 
-    $('table').append(statTable);
+        $('#statTable').append(statTable);
 
-});
+    });
 
 };
 
-function getMatchStats (inputValue, platformValue){
-
-
-var settingsMatch = {
-    "async": true,
-    "crossDomain": true,
-    "url": `https://call-of-duty-modern-warfare.p.rapidapi.com/warzone-matches/${inputValue}/${platformValue}`,
-    "method": "GET",
-    "headers": {
-        "x-rapidapi-host": "call-of-duty-modern-warfare.p.rapidapi.com",
-        "x-rapidapi-key": "3d89a34d3cmshc035eb7f8e6a3f2p12c4fcjsnea2565e57614"
+function getMatchStats(inputValue, platformValue) {
+    var settingsMatch = {
+        "async": true,
+        "crossDomain": true,
+        "url": `https://call-of-duty-modern-warfare.p.rapidapi.com/warzone-matches/${inputValue}/${platformValue}`,
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "call-of-duty-modern-warfare.p.rapidapi.com",
+            "x-rapidapi-key": "3d89a34d3cmshc035eb7f8e6a3f2p12c4fcjsnea2565e57614"
+        }
     }
-}
 
-$.ajax(settingsMatch).then(function (response) {
+    $.ajax(settingsMatch).then(function (responseM) {
 
-    console.log(response);
+        
+        var matchRes = responseM.summary.all;
+        var mKills = matchRes.kills;
+        var gulKills = matchRes.gulagKills;
+        var headPer = matchRes.headshotPercentage;
+        var headPerConv = headPer * 100
+        var headPerConvLim = headPerConv.toFixed(3)
+        var mKd = matchRes.kdRatio;
+        var mKdCap = mKd .toFixed(3)
+        var killsPerGame = matchRes.killsPerGame;
+        var teamsWiped = matchRes.objectiveTeamWiped;
+        var mtimePlayed = matchRes.timePlayed;
+        console.log(matchRes)
 
-});
+        var matchTable = `<tr> 
+        <td> ${inputValue} </td>
+        <td>${mtimePlayed} </td> 
+        <td>Wins Holder </td> 
+        <td>${headPerConvLim} </td> 
+        <td> ${mKills} </td> 
+        <td> ${gulKills} </td> 
+        <td> ${killsPerGame}</td> 
+        <td> ${mKdCap} </td> 
+        <td> ${teamsWiped} </td>   
+        </tr>`;
+
+        $('#mTable').append(matchTable);
+
+    });
 
 };
 
@@ -72,9 +98,9 @@ searchButton.addEventListener('click', function (event) {
     var platformValue = platInput.value;
 
     getCurrentStats(inputValue, platformValue);
-        setTimeout(() => {
-            getMatchStats (inputValue, platformValue);
-        }, 3000);
+    setTimeout(() => {
+    getMatchStats(inputValue, platformValue);
+    }, 3000);
 
     searchInput.value = '';
     platInput.value = '';
